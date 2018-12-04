a803706ed15efd11275fbb82453f67cb00a12e5a
dengdingwen <dengdingwen001@ke.com>
2018-12-04 11:33:36 +0800

    添加leetcode

---
+++ forOffer/src/leetcode/LengthOfLongestSubstring.java
@@ -0,0 +1,43 @@
+/**
+ * \$Id: LengthOfLongestSubstring.java,v 1.0 2018/12/4 10:23 dengdingwen
+ *
+ * @Copyright (c) 2018/12/4, Lianjia Group All Rights Reserved.
+ */
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 类注释，描述
+ *
+ * @author dengdingwwen
+ * @version $Id: LengthOfLongestSubstring.java,v 1.0 2018/12/4 10:23 dengdingwwen
+ * @date 2018/12/4 10:23
+ */
+public class LengthOfLongestSubstring {
+
+    //滑动窗口
+    //我们可以定义字符到索引的映射，而不是使用集合来判断一个字符是否存在。
+    // 当我们找到重复的字符时，我们可以立即跳过该窗口。
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == null || s.equals("")) {
+            return 0;
+        }
+        int maxLength = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int end = 0, start = 0; end < s.length(); end++) {
+            if (map.containsKey(s.charAt(end))) {
+                start = Math.max(map.get(s.charAt(end)), start);
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
+            map.put(s.charAt(end), end + 1);
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+
+}
--- forOffer/src/leetcode/TwoSum.java
+++ forOffer/src/leetcode/TwoSum.java
@@ -24,6 +24,11 @@ public class TwoSum {
         return null;
     }
 
+    //在进行迭代并将元素插入到表中的同时，我们还会回过头来检查表中是否已经存在当前元素所对应的目标元素。
+    // 如果它存在，那我们已经找到了对应解，并立即将其返回。
+    //1、确定当前数字，需要再加上哪个数字才可组成目标数字
+    //2、原来的map中是否有这个数字
+    //3、有即是目标结果，没有则继续找
     public int[] twoSum2(int[] nums, int target){
         HashMap<Integer, Integer> map = new HashMap<>();
         int res[] = new int[2];
